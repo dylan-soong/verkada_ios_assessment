7521de71ecad96d75d0cde773cbdfa698d29874b
Dylan Soong <dylan.soong@gmail.com>
2025-11-06 10:27:01 +0900

    Initial Commit

---
+++ Verkada iOS Assessment/Verkada_iOS_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Verkada_iOS_AssessmentApp.swift
+//  Verkada iOS Assessment
+//
+//  Created by Dylan Soong on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Verkada_iOS_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
